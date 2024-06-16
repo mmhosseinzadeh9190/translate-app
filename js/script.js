6de5e4b7d2a962f212b497bf64aa7cda259fe15a
Mohammad Mahdi Hosseinzadeh <mohammadmahdihosseinzadeh68@gmail.com>
2024-06-16 19:56:39 +0330

    chore: added dropdown functionality

--- js/script.js
+++ js/script.js
@@ -1,1 +1,23 @@
 "use strict";
+
+const dropdown = document.querySelectorAll(".dropdown");
+const dropdownIcon = document.querySelectorAll(".dropdown__icon");
+const dropdownMenu = document.querySelectorAll(".dropdown__menu");
+const textarea = document.querySelectorAll(".text-area");
+const textareaIcon = document.querySelector(".icon-box--text-area");
+
+window.addEventListener("click", function (e) {
+  for (let i = 0; i < dropdown.length; i++) {
+    if (e.target === dropdown[i] || e.target === dropdownIcon[i]) {
+      dropdownIcon[i].classList.toggle("rotate");
+      dropdownMenu[i].classList.toggle("show");
+      textarea[i].classList.toggle("color-transparent");
+      if (i === 0) textareaIcon.classList.toggle("hidden");
+    } else {
+      dropdownIcon[i].classList.remove("rotate");
+      dropdownMenu[i].classList.remove("show");
+      textarea[i].classList.remove("color-transparent");
+      if (i === 0) textareaIcon.classList.remove("hidden");
+    }
+  }
+});
